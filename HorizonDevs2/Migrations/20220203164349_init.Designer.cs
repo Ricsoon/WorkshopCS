e915a779d9d139065f84f016e276bd2082fad2de
Ricsoon <painonex2@gmail.com>
2022-02-03 13:45:26 -0300

    Recriando migration com carga inicial

--- HorizonDevs2/Migrations/20220203164349_init.Designer.cs
+++ HorizonDevs2/Migrations/20220203164349_init.Designer.cs
@@ -2,14 +2,16 @@
 using HorizonDevs2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HorizonDevs2.Migrations
 {
     [DbContext(typeof(SmartContext))]
-    partial class SmartContextModelSnapshot : ModelSnapshot
+    [Migration("20220203164349_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
